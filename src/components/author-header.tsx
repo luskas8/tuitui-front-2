35487faf84830acb5ca38f4bb03cd2e418259049
Lucas Anjos <42415782+luskas8@users.noreply.github.com>
2023-05-18 11:24:53 -0300

    edit-author modal (#5)

--- src/components/author-header.tsx
+++ src/components/author-header.tsx
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom";
-import { BUTTON_GROUP_ORIENTATION, Button, ButtonGroup } from "./buttons";
 import { ReactComponent as ArrowLeft } from "../assets/icons/ArrowLeft.svg";
-import { ReactComponent as Plus } from "../assets/icons/Plus.svg";
+import { BUTTON_GROUP_ORIENTATION, Button, ButtonGroup } from "./buttons";
+// import { ReactComponent as Plus } from "../assets/icons/Plus.svg";
 import { ReactComponent as Edit } from "../assets/icons/Edit.svg";
+import { useModal } from "../hooks/useModal";
+import Modal from "./modal";
 
 
 interface AuthorHeaderProps {
@@ -15,8 +17,11 @@ interface AuthorHeaderProps {
 
 export default function AuthorHeader({ author }: AuthorHeaderProps) {
   const navigate = useNavigate();
+  const { toggleVisibility } = useModal();
+
   return (
     <div className="bg-white w-full h-full py-3 px-6 rounded-sm">
+      <EditAuthorProfileModal />
       <nav className="flex items-center gap-2 py-1 mb-2 border-b-2 border-slate-100">
         <Button.Default onClick={() => navigate(-1)} className="w-5 h-5">
           <ArrowLeft />
@@ -29,11 +34,11 @@ export default function AuthorHeader({ author }: AuthorHeaderProps) {
           <h2 className="mt-1 font-light text-slate-500">{author.username}</h2>
         </section>
         <ButtonGroup orientation={BUTTON_GROUP_ORIENTATION.VERTICAL}>
-          <Button small>
+          {/* <Button small>
             <Plus />
             Seguir
-          </Button>
-          <Button.Outline small>
+          </Button> */}
+          <Button.Outline small onClick={toggleVisibility}>
             <Edit />
             Editar
           </Button.Outline>
@@ -44,3 +49,35 @@ export default function AuthorHeader({ author }: AuthorHeaderProps) {
   );
 }
 
+function EditAuthorProfileModal() {
+  return (
+    <Modal>
+      <Modal.Header>
+        <Modal.Title>Editar perfil</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="flex flex-col gap-4">
+          <div className="input-group border border-slate-300 text-slate-400 rounded-sm flex flex-col px-1 py-2 focus-within:border-violet-400 focus-within:text-violet-400">
+            <label className="" htmlFor="author-name">Nome</label>
+            <input className="focus:outline-none text-black" type="text" id="author-name" />
+          </div>
+          <div className="input-group border border-slate-300 text-slate-400 rounded-sm flex flex-col px-1 py-2 focus-within:border-violet-400 focus-within:text-violet-400">
+            <label htmlFor="author-bio">Bio</label>
+            <textarea className="focus:outline-none text-black resize-none" id="author-bio" />
+          </div>
+          <div>
+            <h3 className="font-bold text-sm">Configurações de dados</h3>
+            <ButtonGroup className="mt-4" orientation={BUTTON_GROUP_ORIENTATION.VERTICAL}>
+              {/* <Button.Outline small>Alterar senha</Button.Outline>
+              <Button.Outline small>Baixar dados</Button.Outline> */}
+              <Button.Danger small>Excluir conta</Button.Danger>
+            </ButtonGroup>
+          </div>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button.Success>Salvar</Button.Success>
+      </Modal.Footer>
+    </Modal>
+  );
+}
